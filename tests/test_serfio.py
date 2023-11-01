ea4e8ef004d85f09c7c1692034ffeab6399a7ed7
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-01 15:36:40 +0400

    added asyncio import and stream test to serfio tests

--- tests/test_serfio.py
+++ tests/test_serfio.py
@@ -1,3 +1,6 @@
+import asyncio
+
+
 async def test_stats(serf):
     async with serf:
         header, body = await serf.stats()
@@ -16,3 +19,20 @@ async def test_event(serf):
     async with serf:
         header = await serf.event('test', 'test')
         assert not header['Error']
+
+
+async def test_stream(serf):
+    async with serf:
+        async def serf_event():
+            await asyncio.sleep(0.1)
+            header = await serf.event('test', 'test')
+            assert not header['Error']
+        serf_event = asyncio.create_task(serf_event())
+
+        async for event in serf.stream():
+            header, body = event
+            assert not header['Error']
+            assert body['Event'] == 'user'
+            assert body['Name'] == 'test'
+            assert body['Payload'] == b'test'
+            break

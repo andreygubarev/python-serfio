11625bcf6e2c017b792e0338edb78f3a4be9a7fe
Andrey Gubarev <andrey@andreygubarev.com>
2023-11-01 13:13:09 +0400

    added async test for 'members' method in serfio tests, checking for errors and response content

--- tests/test_serfio.py
+++ tests/test_serfio.py
@@ -3,3 +3,10 @@ async def test_stats(serf):
         header, body = await serf.stats()
         assert not header['Error']
         assert 'serf' in body
+
+
+async def test_members(serf):
+    async with serf:
+        header, body = await serf.members()
+        assert not header['Error']
+        assert 'Members' in body
